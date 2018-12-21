739641721757ee3791cfa13e76099b666687ef3d
ihollander <ianholla@gmail.com>
2018-12-21 15:48:39 -0500

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,16 @@
 export default (state = [], action) => {
-  return state;
+  switch (action.type) {
+    case "ADD_QUOTE":
+      return [...state, action.quote]
+    case "REMOVE_QUOTE":
+      return state.filter(q => q.id !== action.quoteId)
+    case "UPVOTE_QUOTE":
+      return state.map(q => q.id === action.quoteId ? {...q, votes: q.votes + 1} : q)
+    case "DOWNVOTE_QUOTE":
+      const quote = state.find(q => q.id === action.quoteId)
+      if (quote.votes === 0) return state
+      return state.map(q => q.id === action.quoteId ? {...q, votes: q.votes - 1} : q)
+    default:
+      return state
+  }
 }
