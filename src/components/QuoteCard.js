3617b83108d2352f1dd95717b79a48db4c55a8f8
magic8baller <lturis@gmail.com>
2019-02-23 20:45:43 -0500

    donez but hadda move createStore in index to get devtools workin..?

--- src/components/QuoteCard.js
+++ src/components/QuoteCard.js
@@ -1,38 +1,50 @@
-import React from 'react';
+import React from 'react'
 
-const QuoteCard = (props) =>
-  <div>
-    <div className="card card-inverse card-success card-primary mb-3 text-center">
-      <div className="card-block">
-        <blockquote className="card-blockquote">
-          {/* <p>{Render Quote Content}</p> */}
-          {/* <footer>- author <cite title="Source Title">{Render Quote Author}</cite></footer> */}
-        </blockquote>
-      </div>
-      <div className="float-right">
-        <div className="btn-group btn-group-sm" role="group" aria-label="Basic example">
-          <button
-            type="button"
-            className="btn btn-primary"
-          >
-            Upvote
-          </button>
-          <button
-            type="button"
-            className="btn btn-secondary"
-          >
-            Downvote
-          </button>
-          <button
-            type="button"
-            className="btn btn-danger"
-          >
-            <span aria-hidden="true">&times;</span>
-          </button>
+const QuoteCard = ({ quote, removeQuote, upvoteQuote, downvoteQuote }) => {
+  const { id, author, content, votes } = quote
+  return (
+    <div>
+      <div className="card card-inverse card-success card-primary mb-3 text-center">
+        <div className="card-block">
+          <blockquote className="card-blockquote">
+            <p key={id}>{content}</p>
+            <footer>
+              - author <cite title="Source Title">{author}</cite>
+            </footer>
+          </blockquote>
         </div>
-        {/* <div>Votes: {Render Quote Votes}</div> */}
+        <div className="float-right">
+          <div
+            className="btn-group btn-group-sm"
+            role="group"
+            aria-label="Basic example"
+          >
+            <button
+              onClick={() => upvoteQuote(id)}
+              type="button"
+              className="btn btn-primary"
+            >
+              Upvote
+            </button>
+            <button
+              onClick={() => downvoteQuote(id)}
+              type="button"
+              className="btn btn-secondary"
+            >
+              Downvote
+            </button>
+            <button
+              onClick={() => removeQuote(id)}
+              type="button"
+              className="btn btn-danger"
+            >
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div>Votes: {votes}</div>
+        </div>
       </div>
     </div>
-  </div>;
-
-export default QuoteCard;
+  )
+}
+export default QuoteCard
